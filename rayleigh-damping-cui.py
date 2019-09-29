fddcb3823b00804d2b640dc9fe5e9cf25e207d14
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-09-29 19:29:30 +0900

    レイリー減衰のCUIバージョンを作成

---
+++ rayleigh-damping-cui.py
@@ -0,0 +1,37 @@
+import os.path
+import sys
+import json
+import numpy as np
+
+
+def printhelp():
+    print("python rayleigh-damping-cui.py [configure json file]")
+    sys.exit()
+
+
+def computerayreigh(js):
+    fa = float(js["low freq"])
+    fb = float(js["high freq"])
+    da = float(js["low damp"])
+    db = float(js["high damp"])
+
+    oa = 2. * np.pi / fa
+    ob = 2. * np.pi / fb
+    under = ob**2. - oa**2.
+    a = (2. * oa * ob (da * ob - db * oa)) / under
+    b = (2. * (db * ob - da * oa)) / under
+    return (a, b)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        printhelp()
+    
+    confpath = sys.argv[1]
+    if not os.path.exists(confpath):
+        printhelp()
+    
+    with open(confpath, "r") as f:
+        js = json.load(f)
+    
+    print(js)
